eccd3ebb907b6f186f455639a94bea222abe7c07
bramtechs <bramtech@telenet.be>
2025-04-22 16:15:32 +0200

    small docker example with express and build step

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
-# Stage 1: Build the application
-FROM node:18-alpine AS builder
+# builder
+FROM node:20-alpine AS builder
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
 
-# Stage 2: Create the production image
-FROM alpine:latest
+# runner
+FROM node:20-alpine
 WORKDIR /app
-COPY --from=builder /app/dist .
-COPY start.sh .
-EXPOSE 3000
-CMD ["./start.sh"]
+COPY --from=builder /app/dist/server.js ./
+COPY --from=builder /app/package*.json ./
+RUN npm install --production
+EXPOSE 8080
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var http = require('http')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-var port = 4000
+app.get('/', (req, res) => {
+  res.send('Hello World from Docker NodeJS\n');
+});
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
-})
-
-server.listen(port)
-
-console.log('Server running at http://localhost:' + port)
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
